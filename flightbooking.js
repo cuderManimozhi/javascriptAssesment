471e1940ee3fad78dfc85d45a4d8618cf4884e20
Manimozhi <mselvamany@gmail.com>
2022-07-19 00:36:06 +1000

    Booking App

---
+++ flightbooking.js
@@ -0,0 +1,68 @@
+const form = document.getElementById("passngerForm1");
+form.addEventListener("submit", validFormFieldInput);
+var placeSearch, autocomplete;
+
+const retDate = document.getElementById("return");
+const depDate = document.getElementById("depart");
+retDate.addEventListener("change", validateRetDate);
+depDate.addEventListener("change", validateDepDate);
+
+function validateDepDate(event) {
+  event.preventDefault();
+  const departeDate = event.target.value;
+  console.log("DEPART:" + departeDate);
+  const dt = new Date();
+  const today = dt.getFullYear()+"-"+'0'+(dt.getMonth()+1)+"-"+dt.getDate();
+  console.log("TODAY:" + today);
+  if (departeDate < today) {
+    depDate.value=''
+    alert("Depature date  must be Today or a Future date");
+    return false;
+  }
+}
+
+function validateRetDate(event) {
+  event.preventDefault();
+  const retDate = event.target.value;
+  if (retDate < depDate.value) {
+    retDate.value=''
+    alert("Return date must be same as or higher than Departure date");
+    return false;
+  }
+}
+
+function validFormFieldInput(event) {
+  event.preventDefault();
+  const myFormData = new FormData(event.target);
+
+  const formDataObj = Object.fromEntries(myFormData.entries());
+  console.log(formDataObj);
+
+  const roundTrip = document.getElementById("round");
+  if (!formDataObj.returndate && roundTrip.checked) {
+    alert("Return date is required");
+    return false;
+  }
+  if (
+    formDataObj.pfname1 === formDataObj.pfname2 &&
+    formDataObj.plname1 === formDataObj.plname2
+  ) {
+    alert("Passenger 1 and Passenger 2  cannot be same");
+    return false;
+  }
+
+  if (formDataObj.pfname1.length < 5 || formDataObj.pfname2.length < 5) {
+    alert("First name less than 5 characters");
+    return false;
+  }
+  if (formDataObj.age1 < 0 || formDataObj.age2 < 0) {
+    alert("Age of must be greater than 0");
+    return false;
+  }
+  if (!formDataObj.gender1 || !formDataObj.gender2) {
+    alert("Gender is Mandatory");
+    return false;
+  }
+
+  console.log(formDataObj);
+}
